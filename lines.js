f37b340a76fa5f007e83e48b3301b42a7a7c7a3e
Gaute Magnussen <gaute.magnussen@gmail.com>
2018-08-29 14:56:43 +0200

    Code refactoring

--- lines.js
+++ lines.js
@@ -1,44 +1,46 @@
-let doc = document;
-drawing = document.getElementById("logo");
-let size = 100;
-let lines = size / 2;
-let lineWidth = 1;
-let lineWidthSpace = size / lines;
-
-drawing.width = size;
-drawing.height = size;
-ctx = drawing.getContext("2d");
-let i = 0;
-
-let arr = new Uint8Array(lines);
-window.crypto.getRandomValues(arr);
-
-let maxHeight = (i) => {
-    if(i < size / 2) i = i - 1;
-    else i = i + lineWidth;
-    let l = size / 2 - i;
-    return 2 * Math.sqrt(size/2*size/2 - l*l);
-};
-ctx.fillStyle = "#fff";
-
-let loop = () => {
+var lines = () => {
+    drawing = document.getElementById("logo");
+    let size = 100;
+    let lines = size / 2;
+    let lineWidth = 1;
+    let lineWidthSpace = size / lines;
+    
+    drawing.width = size;
+    drawing.height = size;
+    ctx = drawing.getContext("2d");
+    let i = 0;
+    
+    let arr = new Uint8Array(lines);
+    window.crypto.getRandomValues(arr);
     let tempArr = new Uint8Array(1);
-    window.crypto.getRandomValues(tempArr);
-    arr.copyWithin(0,1, lines);
-    arr.set(tempArr, lines - 1);
-    ctx.clearRect(0, 0, size, size);
+    
+    let maxHeight = (i) => {
+        if (i < size / 2) i = i - 1;
+        else i = i + lineWidth;
+        let l = size / 2 - i;
+        return 2 * Math.sqrt(size/2*size/2 - l*l);
+    };
     ctx.fillStyle = "#fff";
     
-    for(i = 0; i < size; i++){
-        let j = arr[i] / 255 * size;
-        let l = Math.min(maxHeight(i * lineWidthSpace), j);
-        ctx.fillRect(i*size/lines, (size/2 - (l/2)), lineWidth, l);
-    }
-    ctx.strokeStyle = "#fff";
-    ctx.beginPath();
-    ctx.arc(size/2,size/2,size/2 - 1,0,2*Math.PI);
-    ctx.lineWidth = 1;
-    ctx.stroke();
-    requestAnimationFrame(loop);   
-};
-loop();
+    let loop = () => {
+        window.crypto.getRandomValues(tempArr);
+        arr.copyWithin(0, 1, lines);
+        arr.set(tempArr, lines - 1);
+        ctx.clearRect(0, 0, size, size);
+        ctx.fillStyle = "#fff";
+        
+        for(i = 0; i < size; i++){
+            let j = arr[i] / 255 * size;
+            let l = Math.min(maxHeight(i * lineWidthSpace), j);
+            ctx.fillRect(i*size/lines, (size/2 - (l/2)), lineWidth, l);
+        }
+        ctx.strokeStyle = "#fff";
+        ctx.beginPath();
+        ctx.arc(size/2,size/2, size/2 - 1, 0, 2*Math.PI);
+        ctx.lineWidth = 1;
+        ctx.stroke();
+        requestAnimationFrame(loop);   
+    };
+    loop();
+}
+lines();
--- noise.js
+++ noise.js
@@ -1,93 +1,61 @@
-function noise(el, fullscreen) {
-    var blocksize = 16;
-    var tilesize = 2;
-    var tiles_w = 34;
-    var tiles_h = 34;
-    var context;
-    var drawing;
-    var offBlockCanvas;
-    var offBlockContext;
-
-    var colors = [
-        [255, 255, 255, 255],
-        [220, 220, 220, 255],
-        [170, 170, 170, 255],
-        [120, 120, 120, 255],
-        [0, 0, 0, 255]
-    ];
-
-    var doc = document;
-
-    drawing = document.getElementById(el);
-    if(fullscreen) {
-        var h = Math.max(
-            doc.body.scrollHeight, doc.documentElement.scrollHeight,
-            doc.body.offsetHeight, doc.documentElement.offsetHeight,
-            doc.body.clientHeight, doc.documentElement.clientHeight
-        );
-        var w = Math.max(
-            doc.body.scrollWidth, doc.documentElement.scrollWidth,
-            doc.body.offsetWidth, doc.documentElement.offsetWidth,
-            doc.body.clientWidth, doc.documentElement.clientWidth
-        );
-        
-        blocksize = Math.floor(h / 2);
-        tilesize = Math.floor(blocksize / 8);
+let noise = () => {
+    let colors = [255,160,120,80,40,20,0,0];
     
-        tiles_w = Math.floor(w / tilesize);
-        tiles_h = Math.floor(h / tilesize);
-
-        drawing.width = w;
-        drawing.height = h;
-    } else {
-        drawing.width = 34;
-        drawing.height = 34;  
-    }
-
-
-    context = drawing.getContext("2d");
+    let drawing = document.getElementById("bg");
+    let h = Math.max(
+        document.body.scrollHeight, document.documentElement.scrollHeight,
+        document.body.offsetHeight, document.documentElement.offsetHeight,
+        document.body.clientHeight, document.documentElement.clientHeight
+    );
+    let w = Math.max(
+        document.body.scrollWidth, document.documentElement.scrollWidth,
+        document.body.offsetWidth, document.documentElement.offsetWidth,
+        document.body.clientWidth, document.documentElement.clientWidth
+    );
+    
+    let blocksize = Math.floor(h / 2);
+    let tilesize = Math.floor(blocksize / 8);
+    
+    let tiles_w = Math.floor(w / tilesize);
+    let tiles_h = Math.floor(h / tilesize);
+    
+    drawing.width = w;
+    drawing.height = h;
+    
+    let context = drawing.getContext("2d");
     context.beginPath();
     context.rect(0, 0, w, h);
     context.fillStyle = 'black';
     context.fill();
-    offBlockCanvas = document.createElement("canvas");
-    var offBlockContext = offBlockCanvas.getContext("2d");
+    let offBlockCanvas = document.createElement("canvas");
+    let offBlockContext = offBlockCanvas.getContext("2d");
     offBlockCanvas.width = blocksize;
     offBlockCanvas.height = blocksize;
-
+    
     imageData = offBlockContext.getImageData(0, 0, blocksize, blocksize);
-    var imagedata = imageData.data;
-
+    let imagedata = imageData.data;
+    
     for (i = 0; i < blocksize * blocksize * 4; i += 4) {
-        var pixelcolor = parseInt(Math.random() * 8)
-
-        if (pixelcolor > 3) {
-            pixelcolor = 4;
-        }
-
-        imagedata[i] = colors[pixelcolor][0];
-        imagedata[i + 1] = colors[pixelcolor][1];
-        imagedata[i + 2] = colors[pixelcolor][2];
-        imagedata[i + 3] = colors[pixelcolor][3];
+        let pixelcolor = parseInt(Math.random() * 8)
+        imagedata[i] = colors[pixelcolor];
+        imagedata[i + 1] = colors[pixelcolor];
+        imagedata[i + 2] = colors[pixelcolor];
+        imagedata[i + 3] = 255;
     }
-
+    
     offBlockContext.putImageData(imageData, 0, 0);
-
-    var loopnoise = function() {
+    
+    let loopnoise = () => {
         requestAnimationFrame(loopnoise);
-        var i;
-        for (i = 0; i <= tiles_h; i++) {
+        for (let i = 0; i <= tiles_h; i++) {
             for (j = 0; j <= tiles_w; j++) {
-                var next_x = Math.floor(Math.random() * (blocksize - tilesize))
-                var next_y = Math.floor(Math.random() * (blocksize - tilesize))
+                let next_x = Math.floor(Math.random() * (blocksize - tilesize))
+                let next_y = Math.floor(Math.random() * (blocksize - tilesize))
                 context.drawImage(offBlockCanvas, next_x, next_y, tilesize, tilesize, j * tilesize, i * tilesize, tilesize, tilesize);
             }
         }
     }
-
     loopnoise();
-}
+ }
 
-
-//noise("logo");
-noise("bg", true);
+noise();
