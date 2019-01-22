ec752b13e95d172e49f9b4de4c2a0e20fa419bfe
Gaute Magnussen <gaute.magnussen@gmail.com>
2019-01-22 08:46:28 +0100

    testing out Service worker

---
+++ sw.js
@@ -0,0 +1,23 @@
+self.addEventListener('install', function(event){
+    event.waitUntil(self.skipWaiting());
+  });
+  
+  self.addEventListener('activate', function(event) {
+    event.waitUntil(self.clients.claim());
+  });
+  
+  self.addEventListener('fetch', function(event){
+    event.respondWith(caches.match(event.request)
+      .then(function(response){
+        if(response){
+          return response;
+        }
+        return fetch(event.request).then(function(res){
+          var res2 = res.clone();
+          caches.open('horse-app').then(function(cache){
+            cache.put(event.request, res2);
+          });
+          return res;
+        });
+      }));
+  });
